758023b7861e1940c28a248c28aa226cdb822e90
Reed  Harston <rtharston@gmail.com>
2023-01-08 07:56:48 -0800

    Made pixel a little more generic

--- Sources/RayTracingWeekend/Image/Pixel.swift
+++ Sources/RayTracingWeekend/Image/Pixel.swift
@@ -1,10 +1,15 @@
-public struct Pixel {
-    var r: UInt8
-    var g: UInt8
-    var b: UInt8
-    var a: UInt8
+typealias Pixel = GenericPixel<UInt8>
 
-    public init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8 = 255) {
+struct GenericPixel<Size: UnsignedInteger> {
+    let r: Size
+    let g: Size
+    let b: Size
+    let a: Size
+
+}
+
+extension GenericPixel where Size == UInt8 {
+    init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8 = 255) {
         r = red
         g = green
         b = blue
