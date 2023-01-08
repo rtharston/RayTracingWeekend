157c76c36da5b2f2ce6c7125cf552e8da0d2eb56
Reed  Harston <rtharston@gmail.com>
2023-01-07 21:52:07 -0800

    Simplify PPM

--- Sources/RayTracingWeekend/Image/PPM.swift
+++ Sources/RayTracingWeekend/Image/PPM.swift
@@ -1,22 +1,11 @@
 extension RTImage {
     func renderToPPM() -> String {
-        var output = "P3\n\(width) \(height)\n255\n"
-
-        for (i, pixel) in pixels.enumerated() {
-            output += pixel.asPPMString
-            // TODO: Consider using chunked from swift-algorithms
-            if i % width == 0 {
-                output += "\n"
-            }
-            // this is trusting that the pixel array is the right size
-        }
-
-        return output
+        "P3\n\(width) \(height)\n255\n" + pixels.map(\.asPPMString).joined(separator: "\n")
     }
 }
 
 fileprivate extension Pixel {
     var asPPMString: String {
-        "\(r) \(g) \(b) "
+        "\(r) \(g) \(b)"
     }
 }
