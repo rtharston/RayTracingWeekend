832e225fce527f2a1c6a03d9fbd707b52bc69fa7
Reed  Harston <rtharston@gmail.com>
2023-01-18 07:22:49 -0800

    print the whole image at once to make saving much faster

--- Rust/src/output/ppm.rs
+++ Rust/src/output/ppm.rs
@@ -6,12 +6,8 @@ impl crate::Pixel {
 
 impl crate::Image {
     pub fn print_ppm(self) {
-        println!("P3");
-        println!("{} {}", self.get_width(), self.get_height());
-        println!("255");
-
-        for pixel in self.get_pixels() {
-            println!("{}", pixel.ppm_string())
-        }
+        let ppm_header = format!("P3\n{} {}\n255\n", self.get_width(), self.get_height());
+        let ppm_string = self.get_pixels().iter().map(|x| x.ppm_string() + "\n").collect::<String>();
+        print!("{}{}", ppm_header, ppm_string);
     }
 }
