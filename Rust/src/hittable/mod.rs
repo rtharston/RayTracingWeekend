2e6217ae87fb88886034ec830b0076521208ef16
Reed  Harston <rtharston@gmail.com>
2023-01-20 06:10:29 -0800

    Created Hittable trait

---
+++ Rust/src/hittable/mod.rs
@@ -0,0 +1,26 @@
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
+
+pub struct HitRecord {
+    p: Point3,
+    normal: Vec3,
+    t: f64,
+}
+
+impl HitRecord {
+    pub fn get_p(&self) -> &Point3 {
+        &self.p
+    }
+
+    pub fn get_normal(&self) -> &Vec3 {
+        &self.normal
+    }
+
+    pub fn get_t(&self) -> &f64 {
+        &self.t
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
