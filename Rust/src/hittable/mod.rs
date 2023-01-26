fd6bdbb52333808f741ae8eed06bb8fbea65cc43
Reed  Harston <rtharston@gmail.com>
2023-01-25 18:45:45 -0800

    Implemented Hittable for Vec<Hittable>.

--- Rust/src/hittable/mod.rs
+++ Rust/src/hittable/mod.rs
@@ -4,6 +4,7 @@ pub use sphere::Sphere;
 use crate::ray::Ray;
 use crate::vec3::{Point3, Vec3};
 
+#[derive(Clone, Copy)]
 pub struct HitRecord {
     p: Point3,
     normal: Vec3,
@@ -48,4 +49,21 @@ impl HitRecord {
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
     // fn hit(&self, ray: &Ray) -> Option<HitRecord>;
+}
+
+impl<H: Hittable> Hittable for std::vec::Vec<H> {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut temp_rec: Option<HitRecord> = None;
+        let mut closest_so_far = t_max;
+
+        for hittable_object in self.iter() {
+            let hit_record = hittable_object.hit(ray, t_min, closest_so_far);
+            if let Some(record) = hit_record {
+                closest_so_far = record.t;
+                temp_rec = hit_record;
+            }
+        }
+
+        return temp_rec;
+    }
 }
