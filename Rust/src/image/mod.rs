f159565b19f19734f5fd8dd37cdbd33c7f4f9f4a
Reed  Harston <rtharston@gmail.com>
2023-02-24 22:52:01 -0800

    Simplified ray traced image generator and Image::new now that I don't have the ability to select the pixel generator. This lets me create the world Vec just once, and not once per pixel.

--- Rust/src/image/mod.rs
+++ Rust/src/image/mod.rs
@@ -9,26 +9,7 @@ pub struct Image {
 }
 
 impl Image {
-    pub fn new(
-        width: usize,
-        height: usize,
-        pixel_generator: &dyn Fn(usize, usize, usize, usize) -> Pixel,
-    ) -> Image {
-        let mut pixels = Vec::<Pixel>::with_capacity(width * height);
-
-        let mut progress_counter = 0;
-        for h in (0..height).rev() {
-            if progress_counter % 10 == 0 && h != 0 {
-                eprintln!("Scanlines remaining: {}", h);
-            }
-            progress_counter += 1;
-
-            for w in 0..width {
-                pixels.push(pixel_generator(width, height, w, h))
-            }
-        }
-        eprintln!("Render complete.");
-
+    pub fn new(pixels: Vec<Pixel>, width: usize, height: usize) -> Image {
         Image {
             pixels,
             width,
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -14,7 +14,7 @@ use hittable::{Hittable, Sphere};
 // I want this to be in main (and selectable) and passed in to the generator
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 
-fn first_ray_traced_gen(width: usize, height: usize, w: usize, h: usize) -> Pixel {
+fn ray_traced_image(width: usize, height: usize) -> Image {
     const VIEWPORT_HEIGHT: f64 = 2.0;
     const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
     const FOCAL_LENGTH: f64 = 1.0;
@@ -25,32 +25,45 @@ fn first_ray_traced_gen(width: usize, height: usize, w: usize, h: usize) -> Pixe
     let lower_left_corner: Vec3 =
         ORIGIN - HORIZONTAL / 2.0 - VERTICAL / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
 
-    // TODO: refactor the generator to only create the hittable objects once.
     let world: Vec<Sphere> = vec![
         Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5),
         Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0),
     ];
 
-    let u = (w as f64) / (width - 1) as f64;
-    let v = (h as f64) / (height - 1) as f64;
-    let ray = Ray::new(
-        ORIGIN,
-        lower_left_corner + u * HORIZONTAL + v * VERTICAL - ORIGIN,
-    );
+    let mut pixels = Vec::<Pixel>::with_capacity(width * height);
 
-    let color = |ray: Ray| -> Color {
-        if let Some(hit) = world.hit(&ray, 0.0, f64::INFINITY) {
-            let n = hit.get_normal();
-            // Return a visualization of the sphere's normal map
-            return 0.5 * Color::new(n.get_x() + 1.0, n.get_y() + 1.0, n.get_z() + 1.0);
+    let mut progress_counter = 0;
+    for h in (0..height).rev() {
+        if progress_counter % 10 == 0 && h != 0 {
+            eprintln!("Scanlines remaining: {}", h);
         }
+        progress_counter += 1;
 
-        let unit_direction = ray.get_direction().unit_vector();
-        let t = 0.5 * (unit_direction.get_y() + 1.0);
-        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
-    };
+        for w in 0..width {
+            let u = (w as f64) / (width - 1) as f64;
+            let v = (h as f64) / (height - 1) as f64;
+            let ray = Ray::new(
+                ORIGIN,
+                lower_left_corner + u * HORIZONTAL + v * VERTICAL - ORIGIN,
+            );
 
-    Pixel::from_color(color(ray))
+            let color = |ray: Ray| -> Color {
+                if let Some(hit) = world.hit(&ray, 0.0, f64::INFINITY) {
+                    let n = hit.get_normal();
+                    // Return a visualization of the sphere's normal map
+                    return 0.5 * Color::new(n.get_x() + 1.0, n.get_y() + 1.0, n.get_z() + 1.0);
+                }
+
+                let unit_direction = ray.get_direction().unit_vector();
+                let t = 0.5 * (unit_direction.get_y() + 1.0);
+                (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+            };
+
+            pixels.push(Pixel::from_color(color(ray)))
+        }
+    }
+    eprintln!("Render complete.");
+    Image::new(pixels, width, height)
 }
 
 enum OutputOption {
@@ -115,5 +128,5 @@ fn main() {
     const WIDTH: usize = 400;
     const HEIGHT: usize = (WIDTH as f64 / ASPECT_RATIO) as usize;
 
-    Image::new(WIDTH, HEIGHT, &first_ray_traced_gen).save(output_option);
+    ray_traced_image(WIDTH, HEIGHT).save(output_option);
 }
