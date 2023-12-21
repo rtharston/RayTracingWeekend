f96a3861727e74e0dbe97cccdcf1bd854e32d45c
Reed  Harston <rtharston@gmail.com>
2023-12-20 21:23:43 -0800

    progress logging

--- CPP/src/InOneWeekend/main.cpp
+++ CPP/src/InOneWeekend/main.cpp
@@ -12,6 +12,7 @@ int main() {
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; ++j) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
       double r = double(i) / (image_width-1);
       double g = double(j) / (image_height-1);
@@ -25,5 +26,7 @@ int main() {
     }
   }
 
+  // the spaces to cover the scanlines remaining message
+  std::clog << "\rDone.                 \n";
   return 0;
 }
