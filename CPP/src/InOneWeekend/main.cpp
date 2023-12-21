8dc4485f3ce10281a430135ada3dbb352b633b5f
Reed  Harston <rtharston@gmail.com>
2023-12-20 21:22:23 -0800

    test image

--- CPP/src/InOneWeekend/main.cpp
+++ CPP/src/InOneWeekend/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello weekend!" << std::endl;
+
+  // Image
+
+  int image_width = 256;
+  int image_height = 256;
+
+  // Render
+
+  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+  for (int j = 0; j < image_height; ++j) {
+    for (int i = 0; i < image_width; ++i) {
+      double r = double(i) / (image_width-1);
+      double g = double(j) / (image_height-1);
+      double b = 0;
+
+      int ir = static_cast<int>(255.99 * r);
+      int ig = static_cast<int>(255.99 * g);
+      int ib = static_cast<int>(255.99 * b);
+
+      std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
+    }
+  }
+
   return 0;
 }
