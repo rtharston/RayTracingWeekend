0bd5cbb77e683255b4554b3fcdf80c4fe3c7a9a1
Reed  Harston <rtharston@gmail.com>
2024-02-13 21:40:00 -0800

    Random double generator

--- CPP/src/InOneWeekend/rtweekend.h
+++ CPP/src/InOneWeekend/rtweekend.h
@@ -2,10 +2,25 @@
 #define RTWEEKEND_H
 
 #include <limits>
+#include <random>
 
 // Constants
 
 constexpr double infinity = std::numeric_limits<double>::infinity();
+
+// Utility Functions
+
+inline double random_double() noexcept {
+  // Returns a random real in [0, 1)
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+
+inline double random_double(const double min, const double max) noexcept {
+  // Returns a random real in [min, max)
+  return min + (max-min)*random_double();
+}
 
 // Common Headers
 
