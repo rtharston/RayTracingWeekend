e39033eec7032be95b0a9076ac46a53102b7984a
Reed  Harston <rtharston@gmail.com>
2024-02-17 14:13:38 -0800

    Hollow glass sphere

--- CPP/src/InOneWeekend/main.cpp
+++ CPP/src/InOneWeekend/main.cpp
@@ -27,10 +27,11 @@ int main(int argc, char* argv[]) {
   const auto material_left   = make_shared<dielectric>(1.5);
   const auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1), 100, material_ground));
-  world.add(make_shared<sphere>(point3( 0.0,      0, -1), 0.5, material_center));
-  world.add(make_shared<sphere>(point3(-1.0,      0, -1), 0.5, material_left));
-  world.add(make_shared<sphere>(point3( 1.0,      0, -1), 0.5, material_right));
+  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1), 100.0, material_ground));
+  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1),   0.5, material_center));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1),   0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1),  -0.4, material_left));
+  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1),   0.5, material_right));
 
   constexpr camera cam(16.0 / 9.0, 400, 100, 50);
   cam.render(world, argc == 1 ? std::cout : fout);
