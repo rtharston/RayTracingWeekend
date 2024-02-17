314cd2236f267ca88ceb972ba01b3ea7faa133c5
Reed  Harston <rtharston@gmail.com>
2024-02-17 14:07:50 -0800

    Added chance for reflection in dielectric

--- CPP/src/InOneWeekend/material.h
+++ CPP/src/InOneWeekend/material.h
@@ -68,7 +68,8 @@ public:
     const bool cannot_refract = refraction_ratio * sin_theta > 1.0;
     vec3 direction;
 
-    if (cannot_refract)
+    // certain ratio cannot refract, and at certain angles there is a chance of reflection
+    if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
       direction = reflect(unit_direction, rec.normal);
     else
       direction = refract(unit_direction, rec.normal, refraction_ratio);
@@ -79,6 +80,13 @@ public:
 
 private:
   const double ir; // Index of refraction
+
+  static double reflectance(double cosine, double ref_idx) {
+    // Use Schlick's approximation for reflectance.
+    auto r0 = (1-ref_idx) / (1+ref_idx);
+    r0 = r0*r0;
+    return r0 + (1-r0)*pow((1-cosine),5);
+  }
 };
 
 // Simple diffused material with rays bounding in completely random directions
