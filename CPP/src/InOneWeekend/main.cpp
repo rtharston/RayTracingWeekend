b17b5642724f5af090aba0fb082b2b265a16bfce
Reed  Harston <rtharston@gmail.com>
2024-02-17 14:59:36 -0800

    cover scene

--- CPP/src/InOneWeekend/main.cpp
+++ CPP/src/InOneWeekend/main.cpp
@@ -22,18 +22,47 @@ int main(int argc, char* argv[]) {
 
   hittable_list world;
 
-  const auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-  const auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-  const auto material_left   = make_shared<dielectric>(1.5);
-  const auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1), 100.0, material_ground));
-  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1),   0.5, material_center));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1),   0.5, material_left));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1),  -0.4, material_left));
-  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1),   0.5, material_right));
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      auto choose_mat = random_double();
+      point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
-  const camera cam(16.0 / 9.0, 400, 100, 50, 90, point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 10, 3.4);
+      if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+        shared_ptr<material> sphere_material;
+
+        if (choose_mat < 0.8) {
+          // diffuse
+          auto albedo = color::random() * color::random();
+          sphere_material = make_shared<lambertian>(albedo);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        } else if (choose_mat < 0.95) {
+          // metal
+          auto albedo = color::random(0.5, 1);
+          auto fuzz = random_double(0, 0.5);
+          sphere_material = make_shared<metal>(albedo, fuzz);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        } else {
+          // glass
+          sphere_material = make_shared<dielectric>(1.5);
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        }
+      }
+    }
+  }
+
+  auto material1 = make_shared<dielectric>(1.5);
+  world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+  auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+  world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+  auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+  world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+  const camera cam(16.0 / 9.0, 400, 10, 50, 20, point3(13,2,3), point3(0,0,0), vec3(0,1,0), 0.6, 10);
   cam.render(world, argc == 1 ? std::cout : fout);
 
   return 0;
