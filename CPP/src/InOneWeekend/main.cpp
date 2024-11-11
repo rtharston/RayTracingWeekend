1f61fc34b86df789af5655558e8b3523581de956
Reed Harston <rtharston@gmail.com>
2024-11-11 06:49:06 -0700

    Remove some unecessary allocations and add some consts in world generation code.

--- CPP/src/InOneWeekend/main.cpp
+++ CPP/src/InOneWeekend/main.cpp
@@ -38,45 +38,43 @@ int main(int argc, char* argv[]) {
   }
 
   hittable_list world;
+  world.objects.reserve(22 * 22);
 
-  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  const auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
   world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+  const auto glass_material = make_shared<dielectric>(1.5);
 
   for (int a = -11; a < 11; a++) {
     for (int b = -11; b < 11; b++) {
-      auto choose_mat = random_double();
+      const auto choose_mat = random_double();
       point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
       if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-        shared_ptr<material> sphere_material;
-
         if (choose_mat < 0.8) {
           // diffuse
-          auto albedo = color::random() * color::random();
-          sphere_material = make_shared<lambertian>(albedo);
-          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+          const auto albedo = color::random() * color::random();
+          world.add(make_shared<sphere>(center, 0.2, make_shared<lambertian>(albedo)));
         } else if (choose_mat < 0.95) {
           // metal
-          auto albedo = color::random(0.5, 1);
-          auto fuzz = random_double(0, 0.5);
-          sphere_material = make_shared<metal>(albedo, fuzz);
-          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+          const auto albedo = color::random(0.5, 1);
+          const auto fuzz = random_double(0, 0.5);
+          world.add(make_shared<sphere>(center, 0.2, make_shared<metal>(albedo, fuzz)));
         } else {
           // glass
-          sphere_material = make_shared<dielectric>(1.5);
-          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+          world.add(make_shared<sphere>(center, 0.2, glass_material));
         }
       }
     }
   }
 
-  auto material1 = make_shared<dielectric>(1.5);
+  const auto material1 = make_shared<dielectric>(1.5);
   world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-  auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+  const auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
   world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-  auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+  const auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
   world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
   const camera cam(16.0 / 10.0, 2560, 20, point3(13,2,3), point3(0,0,0), vec3(0,1,0), 0.6, 10);
